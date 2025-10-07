6322f50ab836c6705be709859b197ff968dc9769
calandradas <calandradudasccj49@gmail.com>
2025-10-08 03:33:40 +0800

    update notion published

--- report_generation.py
+++ report_generation.py
@@ -320,7 +320,7 @@ def generate_report(industry: str = "ai", languages: List[str] = None, skip_mong
             content = f.read()
         reports = {
             #"en":{"content":"Reddit AI Trends Report - 2025-10-06", "title":"Reddit AI Trends Report - 2025-10-06", "timestamp":current_time},
-            "zh":{"content":{content}, "title":"中文标题测试", "timestamp":current_time}
+            "zh":{"content":content, "title":"中文标题测试", "timestamp":current_time}
         }
 
         # Save report to MongoDB
