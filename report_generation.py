46359891c04189ca4d3910947396bafd5b1fcbd2
calandradas <calandradudasccj49@gmail.com>
2025-10-07 22:58:44 +0800

    update notion published

--- report_generation.py
+++ report_generation.py
@@ -312,8 +312,9 @@ def generate_report(industry: str = "ai", languages: List[str] = None, skip_mong
         #if save_to_file:
         #    update_readme_with_latest_report(report_paths)
 
-        latest_path = os.path.join("reports", f"latest_report_{industry}_zh.md")
-        with open(latest_path, 'w', encoding='utf-8') as f:
+        filename = f"rreport_20251007_200655_ai_zh.md"
+        filepath = os.path.join(report_dir, filename)
+        with open(filepath, 'w', encoding='utf-8') as f:
             content = f.read()
         reports = {
             #"en":{"content":"Reddit AI Trends Report - 2025-10-06", "title":"Reddit AI Trends Report - 2025-10-06", "timestamp":current_time},
